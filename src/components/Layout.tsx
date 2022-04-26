844877dd0a9f04a9682cc55b279b05eabd6776d2
theMackabu <theMackabu@gmail.com>
2022-04-26 04:46:26 +0000

    imporve

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,15 +2,10 @@ import React from "react";
 import "twin.macro";
 
 import Main from "./Main";
-import Aside from "./Aside";
-import SidebarMenu from "./SidebarMenu";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div tw="overflow-hidden">
-      <Aside>
-        <SidebarMenu />
-      </Aside>
       <Main>{children}</Main>
     </div>
   );
